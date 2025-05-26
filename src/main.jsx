41b4de9f006badedcc14fce5fb77c2b69e77df1d
LindsellR <lindsellrichard@gmail.com>
2025-05-26 16:55:36 +1000

    Rechecked syntax to debug deployment error

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,17 @@
-// import * as atatus from 'atatus-spa';
-// atatus.config('0e60f1dd7a5b4a8b810f213bea29f028').install();
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
+// atatus.config('1b1ee5665eb943e3bdb71259beabe47a').install();
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-serviceWorkerRegistration.register()
+ <React.StrictMode>
+   <App />
+ </React.StrictMode>,
+);
+serviceWorkerRegistration.register();
