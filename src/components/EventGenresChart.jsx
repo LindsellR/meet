6ed99ca46d21ce61eacf385bddeedebecf31229d
LindsellR <lindsellrichard@gmail.com>
2025-05-28 17:29:53 +1000

    Adjusting piechart

--- src/components/EventGenresChart.jsx
+++ src/components/EventGenresChart.jsx
@@ -1,42 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import React, { useState, useEffect } from 'react'
+import {
+  PieChart,
+  Pie,
+  ResponsiveContainer,
+  Cell,
+  Legend,
+  Tooltip,
+} from 'recharts'
 
 const EventGenresChart = ({ events }) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([])
 
-  const genres = ['React', 'Javascript', 'Node', 'JQuery', 'Angular'];
+  const genres = ['React', 'Javascript', 'Node', 'JQuery', 'Angular']
+
+  useEffect(() => {
+    setData(getData())
+  }, [`${events}`])
 
   const getData = () => {
     return genres.map((genre) => {
       const filteredEvents = events.filter((event) =>
         event.summary.includes(genre)
-      );
+      )
       return {
         name: genre,
         value: filteredEvents.length,
-      };
-    });
-  };
-
-  useEffect(() => {
-    setData(getData());
-  }, [events]); 
+      }
+    })
+    return data
+  }
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180
+    const radius = outerRadius
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null
+  }
 
   return (
-    <ResponsiveContainer width="99%" height={400}>
-      <PieChart>
-        <Pie
-          data={data}
-          dataKey="value"
-          nameKey="name"
-          outerRadius={130}
-          labelLine={false}
-          label={({ percent }) => `${(percent * 100).toFixed(0)}%`}
-          fill="#8884d8"
-        />
-      </PieChart>
-    </ResponsiveContainer>
-  );
+   <ResponsiveContainer width="99%" height={400}>
+     <PieChart>
+       <Pie
+         data={data}
+         dataKey="value"
+         fill="#8884d8"
+         labelLine={false}
+         label={renderCustomizedLabel}
+         outerRadius={150}           
+       />
+     </PieChart>
+   </ResponsiveContainer>
+ );
 };
 
-export default EventGenresChart;
+export default EventGenresChart
