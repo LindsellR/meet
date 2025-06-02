8bb38a28dbcb04fe47906a065040324e0c6adfff
LindsellR <lindsellrichard@gmail.com>
2025-06-02 12:27:14 +1000

    Added google verification html

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -19,7 +19,7 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split((/, | - /))[0]
+      const city = location.split(/, | - /)[0]
       return { city, count }
     })
     return data
@@ -31,19 +31,24 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 40,
           right: 20,
-          bottom: 60,
+          bottom: 70,
           left: -30,
         }}
       >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14}} />
+        <XAxis
+          type="category"
+          dataKey="city"
+          name="City"
+          angle={60}
+          interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
         <YAxis type="number" dataKey="count" name="Number of Events" />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
     </ResponsiveContainer>
-
-    
   )
 }
 
